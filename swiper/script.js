db4198d3709a91e56e140707cb505285c48ddde7
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-06-21 16:35:18 +0200

    prettier

--- swiper/script.js
+++ swiper/script.js
@@ -23,10 +23,4 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
