2c35f7e06a32f02d17e796896eb2d75cb0ed6c34
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-09-28 09:35:59 +0200

    preloader, mobile nav, responsiveness, themes

--- loading_screen/index.js
+++ loading_screen/index.js
@@ -1,24 +1,24 @@
-// const loadingFunFactContainer = document.querySelector('.fun-fact')
-// const funFacts = [
-// 	`<span>Did You Know?</span><br> First ever computer mouse was made out of wood!`,
-// 	`<span>Did You Know?</span><br> 86% of people try to plug in their USB devices upside down.`,
-// 	`<span>Did You Know?</span><br> The first product scanned was a packet of chewing gum in 1974.`,
-// 	`<span>Did You Know?</span><br> “Phantom Vibration Syndrome” is the name for when someone thinks their phone is vibrating, but it isn't.`,
-// 	`<span>Did You Know?</span><br> There is special Braille technology and accessories for blind people to use cell phones.`,
-// 	`<span>Did You Know?</span><br> Over 6,000 new computer viruses are created and released every month.`,
-// 	`<span>Did You Know?</span><br> Comic Sans is the most hated font in the world.`,
-// 	`<span>Did You Know?</span><br> More people have cell phones than toilets.`,
-// 	`<span>Did You Know?</span><br> Facebook pays $500 for reporting any vulnerability in their security.`,
-// 	`<span>Did You Know?</span><br> The first webpage is still running.`,
-// 	`<span>Did You Know?</span><br> Most of today's successful companies started in garages.`,
-// 	`<span>Did You Know?</span><br> About 51% of internet traffic is non-human. Over 30% is from hacking programs, spammers, and phishing.`,
-// 	`<span>Did You Know?</span><br> The first computer virus was harmless.`,
-// ]
-// const loadingFunFacts = funFacts[Math.floor(Math.random() * funFacts.length)]
-// loadingFunFactContainer.innerHTML = loadingFunFacts
+const loadingFunFactContainer = document.querySelector('.fun-fact')
+const funFacts = [
+	`<span>Did You Know?</span><br> First ever computer mouse was made out of wood!`,
+	`<span>Did You Know?</span><br> 86% of people try to plug in their USB devices upside down.`,
+	`<span>Did You Know?</span><br> The first product scanned was a packet of chewing gum in 1974.`,
+	`<span>Did You Know?</span><br> “Phantom Vibration Syndrome” is the name for when someone thinks their phone is vibrating, but it isn't.`,
+	`<span>Did You Know?</span><br> There is special Braille technology and accessories for blind people to use cell phones.`,
+	`<span>Did You Know?</span><br> Over 6,000 new computer viruses are created and released every month.`,
+	`<span>Did You Know?</span><br> Comic Sans is the most hated font in the world.`,
+	`<span>Did You Know?</span><br> More people have cell phones than toilets.`,
+	`<span>Did You Know?</span><br> Facebook pays $500 for reporting any vulnerability in their security.`,
+	`<span>Did You Know?</span><br> The first webpage is still running.`,
+	`<span>Did You Know?</span><br> Most of today's successful companies started in garages.`,
+	`<span>Did You Know?</span><br> About 51% of internet traffic is non-human. Over 30% is from hacking programs, spammers, and phishing.`,
+	`<span>Did You Know?</span><br> The first computer virus was harmless.`,
+]
+const loadingFunFacts = funFacts[Math.floor(Math.random() * funFacts.length)]
+loadingFunFactContainer.innerHTML = loadingFunFacts
 
-// window.addEventListener('load', () => {
-// 	setTimeout(() => {
-// 		document.querySelector('.page-loader').classList.add('loaded')
-// 	}, 3000)
-// })
+window.addEventListener('load', () => {
+	setTimeout(() => {
+		document.querySelector('.preloader').classList.add('loaded')
+	}, 3000)
+})
