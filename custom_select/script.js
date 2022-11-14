2b52f63e61194883fdbd7b1800afc8429b6f30fe
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-14 13:23:24 +0100

    setting current option

--- custom_select/script.js
+++ custom_select/script.js
@@ -1,5 +1,16 @@
 const selectMenu = document.querySelector('.select')
+const options = document.querySelectorAll('.option')
+
+const setOption = (option) => {
+	document.querySelector('.select input').value = option
+}
 
 selectMenu.addEventListener('click', () => {
-  selectMenu.classList.toggle('open')
+	selectMenu.classList.toggle('open')
+})
+
+options.forEach((option) => {
+	option.addEventListener('click', () => {
+		setOption(option.textContent)
+	})
 })
