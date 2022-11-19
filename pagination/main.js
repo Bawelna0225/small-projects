17baee987ae6cbf158b73715b572d2ef5a725fde
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-19 13:16:36 +0100

    moving to last page

--- pagination/main.js
+++ pagination/main.js
@@ -1,89 +1,94 @@
 const list_items = [
-	"Item 1",
-	"Item 2",
-	"Item 3",
-	"Item 4",
-	"Item 5",
-	"Item 6",
-	"Item 7",
-	"Item 8",
-	"Item 9",
-	"Item 10",
-	"Item 11",
-	"Item 12",
-	"Item 13",
-	"Item 14",
-	"Item 15",
-	"Item 16",
-	"Item 17",
-	"Item 18",
-	"Item 19",
-	"Item 20",
-	"Item 21",
-	"Item 22"
-];
+	'Item 1',
+	'Item 2',
+	'Item 3',
+	'Item 4',
+	'Item 5',
+	'Item 6',
+	'Item 7',
+	'Item 8',
+	'Item 9',
+	'Item 10',
+	'Item 11',
+	'Item 12',
+	'Item 13',
+	'Item 14',
+	'Item 15',
+	'Item 16',
+	'Item 17',
+	'Item 18',
+	'Item 19',
+	'Item 20',
+	'Item 21',
+	'Item 22',
+]
 
-const list_element = document.querySelector('.list');
-const pagination = document.querySelector('.pagination-numbers');
+const list_element = document.querySelector('.list')
+const pagination = document.querySelector('.pagination-numbers')
 const firstPage = document.querySelector('.first-page')
+const lastPage = document.querySelector('.last-page')
 
 firstPage.onclick = () => {
-	DisplayList(list_items, list_element, rows, 1);
+	DisplayList(list_items, list_element, rows, 1)
 	document.querySelector('button.active').classList.remove('active')
 	document.querySelectorAll('.pagination-numbers button')[0].classList.add('active')
 }
 
-let current_page = 1;
-let rows = 5;
+lastPage.onclick = () => {
+	let page_count = Math.ceil(list_items.length / rows)
+	DisplayList(list_items, list_element, rows, page_count)
+}
 
-function DisplayList (items, wrapper, rows_per_page, page) {
-	wrapper.innerHTML = "";
-	page--;
+let current_page = 1
+let rows = 5
 
-	let start = rows_per_page * page;
-	let end = start + rows_per_page;
-	let paginatedItems = items.slice(start, end);
+function DisplayList(items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = ''
+	page--
+
+	let start = rows_per_page * page
+	let end = start + rows_per_page
+	let paginatedItems = items.slice(start, end)
 
 	for (let i = 0; i < paginatedItems.length; i++) {
-		let item = paginatedItems[i];
+		let item = paginatedItems[i]
 
-		let item_element = document.createElement('div');
-		item_element.classList.add('item');
-		item_element.innerText = item;
-		
-		wrapper.appendChild(item_element);
+		let item_element = document.createElement('div')
+		item_element.classList.add('item')
+		item_element.innerText = item
+
+		wrapper.appendChild(item_element)
 	}
 }
 
-function SetupPagination (items, wrapper, rows_per_page) {
-	wrapper.innerHTML = "";
+function SetupPagination(items, wrapper, rows_per_page) {
+	wrapper.innerHTML = ''
 
-	let page_count = Math.ceil(items.length / rows_per_page);
+	let page_count = Math.ceil(items.length / rows_per_page)
 	for (let i = 1; i < page_count + 1; i++) {
-		let btn = PaginationButton(i, items);
-		wrapper.appendChild(btn);
+		let btn = PaginationButton(i, items)
+		wrapper.appendChild(btn)
 	}
 }
 
-function PaginationButton (page, items) {
-	let button = document.createElement('button');
-	button.innerText = page;
+function PaginationButton(page, items) {
+	let button = document.createElement('button')
+	button.innerText = page
 
-	if (current_page == page) button.classList.add('active');
+	if (current_page == page) button.classList.add('active')
 
 	button.addEventListener('click', function () {
-		current_page = page;
-		DisplayList(items, list_element, rows, current_page);
+		current_page = page
+		DisplayList(items, list_element, rows, current_page)
 
-		let current_btn = document.querySelector('button.active');
-		current_btn.classList.remove('active');
+		let current_btn = document.querySelector('button.active')
+		current_btn.classList.remove('active')
 
-		button.classList.add('active');
-	});
+		button.classList.add('active')
+	})
 
-	return button;
+	return button
 }
 
-DisplayList(list_items, list_element, rows, current_page);
-SetupPagination(list_items, pagination, rows);
-
+DisplayList(list_items, list_element, rows, current_page)
+SetupPagination(list_items, pagination, rows)
