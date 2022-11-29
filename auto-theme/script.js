d8bd6e352e4ba02c2c95d1189f714a75b7634533
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-29 14:07:28 +0100

    setting & getting local storage

--- auto-theme/script.js
+++ auto-theme/script.js
@@ -1,9 +1,15 @@
-var html = document.getElementsByTagName('html');
-var radios = document.getElementsByName('theme'); 
+var html = document.getElementsByTagName('html')
+var radios = document.getElementsByName('theme')
+
+if (localStorage.getItem('currentTheme') != null || !localStorage.getItem('currentTheme') != []) {
+    html[0].classList.add(localStorage.getItem('currentTheme'))
+	document.getElementById(localStorage.getItem('currentTheme')).checked = true
+}
 
 radios.forEach((radio) => {
-    radio.addEventListener('click', () => {
-        html[0].classList = ''
-        html[0].classList.add(radio.id)
-    })
+	radio.addEventListener('click', () => {
+		html[0].classList = ''
+		html[0].classList.add(radio.id)
+		localStorage.setItem('currentTheme', radio.id)
+	})
 })
