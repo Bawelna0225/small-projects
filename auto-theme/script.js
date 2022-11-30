3133820a92e6fec0b5c50271cbd0c5175e82de9e
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-11-30 12:51:22 +0100

    function for checking if auto theme is on

--- auto-theme/script.js
+++ auto-theme/script.js
@@ -2,7 +2,14 @@ let html = document.getElementsByTagName('html')
 let radios = document.getElementsByName('theme')
 let autoSettings = document.getElementsByName('auto-setting')
 
+function checkAutoTheme() {
+	if (html[0].classList.contains('auto')) {
+		document.querySelector('.auto-theme-settings').style.display = 'block'
 
+	} else {
+		document.querySelector('.auto-theme-settings').style.display = 'none'
+	}
+}
 
 
 
@@ -11,14 +18,17 @@ radios.forEach((radio) => {
 		html[0].classList = ''
 		html[0].classList.add(radio.id)
 		localStorage.setItem('currentTheme', radio.id)
+		checkAutoTheme()
 	})
 })
 
 if (localStorage.getItem('currentTheme') == null || localStorage.getItem('currentTheme') == []) {
 	html[0].classList.add('auto')
 	localStorage.setItem('currentTheme', 'auto')
+	checkAutoTheme()
 } else {
 	html[0].classList.add(localStorage.getItem('currentTheme'))
 	document.getElementById(localStorage.getItem('currentTheme')).checked = true
 
+	checkAutoTheme()
 }
