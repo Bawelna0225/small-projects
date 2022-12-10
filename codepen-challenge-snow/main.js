bde245e78c6f4af31ec56a820152a2479906931c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-10 13:48:06 +0100

    selecting current option

--- codepen-challenge-snow/main.js
+++ codepen-challenge-snow/main.js
@@ -42,8 +42,16 @@ radios.forEach((radio) => {
 const selectMenu = document.querySelector('.select')
 const options = document.querySelectorAll('.option')
 
-
+const setOption = (option) => {
+  document.querySelector('.select input').value = option
+}
 
 selectMenu.addEventListener('click', () => {
   selectMenu.classList.toggle('open')
+})
+
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    setOption(option.textContent)
+  })
 })
