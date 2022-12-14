bff1fd681e37d0ccb1b9f7304e430871b874711c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-14 14:06:10 +0100

    validating form inputs & email

--- card-info-form/main.js
+++ card-info-form/main.js
@@ -11,4 +11,35 @@ checkoutForm.addEventListener('submit', (e) => {
 	validateForm()
 })
 
-function validateForm() {}
+function validateForm() {
+	let errorFlag = false
+	if (cardNumber.value.length < 1 || cardNumber.value.split(' ').join('') == '') {
+		cardNumber.classList.add('error')
+		errorFlag = true
+	}
+	if (expirationDate.value.length < 1 || expirationDate.value.split(' ').join('') == '') {
+		expirationDate.classList.add('error')
+		errorFlag = true
+	}
+	if (!validateEmail(emailInput.value)) {
+		emailInput.classList.add('error')
+		errorFlag = true
+	}
+	if (cvc.value.length < 1 || cvc.value.split(' ').join('') == '') {
+		cvc.classList.add('error')
+		errorFlag = true
+	}
+	if (nameOnCard.value.length < 1 || nameOnCard.value.split(' ').join('') == '') {
+		nameOnCard.classList.add('error')
+		errorFlag = true
+	}
+	if (zipCode.value.length < 1 || zipCode.value.split(' ').join('') == '') {
+		zipCode.classList.add('error')
+		errorFlag = true
+	}
+}
+
+const validateEmail = (emailInput) => {
+	let pattern = /\S+@\S+\.\S+/
+	return pattern.test(emailInput)
+}
