c0046405b161795e8af62d739053a47e872c6cbc
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-25 14:07:46 +0100

    generating password

--- password_generator/script.js
+++ password_generator/script.js
@@ -18,6 +18,30 @@ const generatePassword = () => {
     excludeDuplicate = false,
     passLength = lengthSlider.value;
 
+    options.forEach(option => { // looping through each option's checkbox
+        if(option.checked) { // if checkbox is checked
+            // if checkbox id isn't exc-duplicate && spaces
+            if(option.id !== "exc-duplicate" && option.id !== "spaces") {
+                // adding particular key value from character object to staticPassword
+                staticPassword += characters[option.id];
+            } else if(option.id === "spaces") { // if checkbox id is spaces
+                staticPassword += `  ${staticPassword}  `; // adding space at the beginning & end of staticPassword
+            } else { // else pass true value to excludeDuplicate
+                excludeDuplicate = true;
+            }
+        }
+    });
 
+    for (let i = 0; i < passLength; i++) {
+        // getting random character from the static password
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if(excludeDuplicate) { // if excludeDuplicate is true
+            // if randomPassword doesn't contains the current random character or randomChar is equal 
+            // to space " " then add random character to randomPassword else decrement i by -1
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else { // else add random character to randomPassword
+            randomPassword += randomChar;
+        }
+    }
+    passwordInput.value = randomPassword; // passing randomPassword to passwordInput value
 }
-
