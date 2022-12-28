cf9c0a9bd31e7ee353b8462fe5c695070c197a22
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-28 11:36:02 +0100

    toast details

--- toast_notification/main.js
+++ toast_notification/main.js
@@ -2,6 +2,26 @@ const buttons = document.querySelectorAll('.buttons button')
 const closeButtons = document.querySelectorAll('.toast button span')
 const notifications = document.querySelector('.notifications')
 
+const toastDetails = {
+	success: {
+		icon: 'check_circle',
+		text: 'Success: success toast',
+	},
+	warning: {
+		icon: 'warning',
+		text: 'Warning: warning toast',
+	},
+	error: {
+		icon: 'error',
+		text: 'Error: error toast',
+	},
+	info: {
+		icon: 'info',
+		text: 'Info: info toast',
+	},
+}
+
+
 const createToast = (id) => {
 
 }
