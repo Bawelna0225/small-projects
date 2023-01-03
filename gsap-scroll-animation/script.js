fc7be20e40df8e67846d085a23e1ec975eecaa69
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-03 16:31:03 +0100

    gsap animation

--- gsap-scroll-animation/script.js
+++ gsap-scroll-animation/script.js
@@ -8,3 +8,26 @@ let timeline = new gsap.timeline({
 		scrub: 1,
 	},
 })
+
+cards.forEach((img) => {
+	let rotation = Math.floor(Math.random() * 40) - 20
+	positionX = Math.floor(Math.random() * 300) - 150
+	positionY = Math.floor(Math.random() * 300) - 150
+
+	timeline.from(
+		img,
+		{
+			rotation: rotation,
+			x: positionX,
+			y: positionY,
+		},
+		'-=1'
+	)
+	timeline.to(
+		img,
+		{
+			backgroundSize: '100%',
+		},
+		0.2
+	)
+})
