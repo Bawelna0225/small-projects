c17cc0a49708a5e1df3ac95307b8340152ec6ae8
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-01-07 21:40:44 +0100

    calendar hover effect

---
+++ calendar/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.dates-container').onmousemove = (e) => {
+	for (const date of document.getElementsByClassName('date')) {
+		const rect = date.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top
+
+		date.style.setProperty('--mouse-x', `${x}px`)
+		date.style.setProperty('--mouse-y', `${y}px`)
+	}
+}
