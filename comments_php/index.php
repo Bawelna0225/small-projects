c71c50270a6b32ea51e6a96e471bd7e4dda9a81b
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-02-12 14:07:03 +0100

    styles import, buttons container

--- comments_php/index.php
+++ comments_php/index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Comments</title>
 </head>
 <body>
@@ -14,8 +15,10 @@
     <h3>Add Comment</h3>
     <form action="index.php" method="get">
         <textarea name="comment" id="" cols="30" rows="10" required placeholder='Your Comment'></textarea>
-        <button type="reset">Reset</button>
-        <button type="submit" name='submit'>Submit</button>
+        <div class="buttons">
+            <button type="reset">Reset</button>
+            <button type="submit" name='submit'>Submit</button>
+        </div>
     </form>
     <h3>Comments</h3>
         <?php 
