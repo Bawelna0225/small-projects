b931f2f7b445903afc50daec7095d290e25a78b3
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-23 14:52:08 +0100

    loading & playing new file

--- audio-visualizer/script.js
+++ audio-visualizer/script.js
@@ -7,4 +7,12 @@ const ctx = canvas.getContext('2d')
 let audioSource
 let analyser
 
+file.addEventListener('change', function () {
+	const files = this.files
+	const audio1 = document.getElementById('audio1')
+	audio1.src = URL.createObjectURL(files[0])
+	audio1.load()
+	audio1.play()
+
+})
 
